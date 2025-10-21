715b91c05a8a24507de542cd9491976578f45de7
Reileen00 <baisnabirout001@gmail.com>
2025-10-21 19:36:36 +0530

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -22,6 +22,7 @@ json decode_bencoded_value(const std::string& encoded_value) {
         }
     } } else if(encoded_value[0] == 'i' && encoded_value.back() == 'e') {
         return json(std::stol(encoded_value.substr(1, encoded_value.length()-2)));
+    }
      else {
         throw std::runtime_error("Unhandled encoded value: " + encoded_value);
     }
